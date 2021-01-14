a126083284fb015e3d671fe9b65196f5b055b70e
Miles Mixon <mxm23@drexel.edu>
2021-01-13 20:06:25 -0500

    initial commit to branch. removed sample functions, renamed project, and began adding necessary classes

---
+++ Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BRIM
+{
+    interface Item
+    {
+        enum unit { oz, ml } //standard measurement units
+        enum status { empty, belowPar, belowIdeal, aboveIdeal } //the status of an inventory item
+
+        String Name { get; set; }
+        double Price { get; set; }
+        double LowerEstimate { get; set; }
+        double UpperEstimate { get; set; }
+        double ParLevel { get; set; }
+        double IdealLevel { get; set; }
+        unit Measurement { get; set; }
+        status Status { get; set; }
+
+        void CalculateStatus();
+    }
+}
