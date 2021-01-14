793d0d3809bfb4dc26b7755aa592925149ea46e3
Miles Mixon <mxm23@drexel.edu>
2021-01-13 23:46:13 -0500

    the skeleton of inventory, but no functionality. Unlear on some of the specifics

--- Inventory.cs
+++ Inventory.cs
@@ -10,6 +10,56 @@ namespace BRIM
     {
         public List<Item> ItemList = new List<Item>(); //holds all items registered to this BRIM instance
         public List<Recipe> RecipeList = new List<Recipe>(); //holds all of the recipes for this BRIM instance
+        public string Country;
 
+        public void UpdateItem(Item i)
+        {
+
+        }
+
+        public void AddItem(Item i)
+        {
+
+        }
+
+        public void RemoveItem(Item i)
+        {
+
+        }
+
+        public void PurchseItem(Recipe r)
+        {
+
+        }
+
+        public void GetItemList()
+        {
+
+        }
+
+        public void EmitEvent()
+        {
+
+        }
+
+        public void ListenEvent()
+        {
+
+        }
+
+        public void AddRecipe()
+        {
+
+        }
+
+        public void UpdateRecipe()
+        {
+
+        }
+
+        public void RemoveRecipe()
+        {
+
+        }
     }
 }
