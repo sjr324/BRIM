55c9873ec372660f2b6084dc981f1072dd4778c5
alex <as4377@drexel.edu>
2021-02-23 14:41:07 -0500

    everything borked

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using BRIM;
+using BRIM.BackendClassLibrary;
 
 namespace React.Sample.Webpack.CoreMvc.Controllers
 {
