66293bde0cbc2b08d774b02c7498cc200c0d0010
DESKTOP-OBSFEQK\Armaan A. Bhasin <aab385@drexel.edu>
2021-04-10 18:15:30 -0400

    Updated Inventory Unit Tests to properly mock up DataBase Manager Tag Functionality, as well as updated other various methods of Mockup to behave more accurately, fixed bugs with DeleteTag and Delete Drink, where attempting to Delete Either Before Clearing related Entries in DrinkTags Table would have Resulted in Failure. Made the Tag List a Part of Items in general to make Program more Robust in the Face of Potential Expansion of Item Types Added Unit Test for Overlooked TestCase in Inventory Item Functionality

--- BackendClassUnitTests/InventoryTests.cs
+++ BackendClassUnitTests/InventoryTests.cs
@@ -13,15 +13,32 @@ namespace BackendClassUnitTests
     {
         private List<Item> fakeDBItems;
         private List<Recipe> fakeDBRecipes;
+        private List<Tag> fakeDBTags;
         private int latestItemID;
         private int latestRecipeID;
+        private int latestTagID;
         //create Mock of Database manager
         private Mock<IDatabaseManager> mockDBManager = new Mock<IDatabaseManager>();
         private Inventory inventory;
 
         public InventoryTests()
         {
-            //set up Fake Database Data
+            //set up Mock Database Data
+            fakeDBTags = new List<Tag> {
+                new Tag {
+                    ID = 1,
+                    Name = "Fake Tag 1"
+                },
+                new Tag {
+                    ID = 2,
+                    Name = "Fake Tag 2"
+                },
+                new Tag {
+                    ID = 3,
+                    Name = "Fake Tag 3"
+                }
+            };
+
             fakeDBItems = new List<Item> {
                 new Drink {
                     ID = 1,
@@ -35,8 +52,12 @@ namespace BackendClassUnitTests
                     BottleSize = 200,
                     Brand = "Fake Brand 1",
                     UnitsPerCase = 10,
-                    Vintage = null
+                    Vintage = null,
+                    Tags = new List<Tag> {
+                        fakeDBTags[0]
+                    }
                 },
+                //drink not used in Any Recipes
                 new Drink {
                     ID = 2,
                     Name = "Fake Drink 2",
@@ -49,7 +70,10 @@ namespace BackendClassUnitTests
                     BottleSize = 400,
                     Brand = "Fake Brand 2",
                     UnitsPerCase = 60,
-                    Vintage = null
+                    Vintage = null,
+                    Tags = new List<Tag> {
+                        fakeDBTags[1]
+                    }
                 },
                 new Drink {
                     ID = 3,
@@ -63,7 +87,11 @@ namespace BackendClassUnitTests
                     BottleSize = 200,
                     Brand = "Fake Brand 1",
                     UnitsPerCase = 10,
-                    Vintage = 1998
+                    Vintage = 1998,
+                    Tags = new List<Tag> {
+                        fakeDBTags[0],
+                        fakeDBTags[1]
+                    }
                 },
                 new Drink {
                     ID = 4,
@@ -77,7 +105,10 @@ namespace BackendClassUnitTests
                     BottleSize = 25,
                     Brand = "Fake Brand 3",
                     UnitsPerCase = 14,
-                    Vintage = null
+                    Vintage = null,
+                    Tags = new List<Tag> {
+                        fakeDBTags[2]
+                    }
                 }
             };
 
@@ -132,16 +163,35 @@ namespace BackendClassUnitTests
             //to simulate the autoincrement effect that the Database has for the Ids
             latestItemID = fakeDBItems.Count();
             latestRecipeID = fakeDBRecipes.Count();
+            latestTagID = fakeDBTags.Count();
 
             //Mockup Methods in mockDBManager
             //Mockups for Drink related Methods
             mockDBManager.Setup(x => x.deleteDrink(It.IsAny<Drink>()))
                 .Returns((Drink drink) => {
-                    
+                    bool drinkUnused = true;
+                    //go through all recipe Items and check to see if Drink in used in any of them
+                    //DataBase will (or at least should), prevent deletions of Drinks with connected
+                    //entries in the DrinkRecipes Table
+                    for (int i = 0; i < latestRecipeID; i++) {
+                        for (int j = 0; j < fakeDBRecipes[i].ItemList.Count(); j++) {
+                            if (fakeDBRecipes[i].ItemList[j].Item.ID == drink.ID) {
+                                drinkUnused = false;
+                                break;
+                            }
+                        }
+                        if (!drinkUnused) {
+                            break;
+                        }
+                    }
+
+                    //If a Recipe uses that drink, drink deletion will Fail
+                    if (!drinkUnused) {
+                        return false;
+                    }
+
                     fakeDBItems = fakeDBItems.Where(x => x.ID != drink.ID).ToList();
                     return true;    
-                    //If a delete Drink fails/returns false, it's probably not going to be the 
-                    //Invetory Method's fault
                 });
 
             mockDBManager.Setup(x => x.addDrink(It.IsAny<Drink>()))
@@ -157,7 +207,11 @@ namespace BackendClassUnitTests
                 .Returns((Drink drink) => {
                     int itemToUpdateIndex = fakeDBItems.FindIndex(x => x.ID == drink.ID);
                     if (itemToUpdateIndex != -1) {
+                        //preserve Item's tag's since updateDrink does not change entries within 
+                        //DrinkTags table
+                        List<Tag> tagListHolder = ((Drink) fakeDBItems[itemToUpdateIndex]).Tags;
                         fakeDBItems[itemToUpdateIndex] = drink;
+                        ((Drink) fakeDBItems[itemToUpdateIndex]).Tags = tagListHolder;
                         return true;
                     } else {
                         return false;
@@ -165,6 +219,8 @@ namespace BackendClassUnitTests
                 });
 
             mockDBManager.Setup(x => x.getDrinks()).Returns(fakeDBItems);
+
+            mockDBManager.Setup(x => x.getTags()).Returns(fakeDBTags);
 
             mockDBManager.Setup(x => x.deleteRecipe(It.IsAny<int>()))
                 .Returns((int recipeID) => {
@@ -216,6 +272,55 @@ namespace BackendClassUnitTests
                     }
                 });
 
+            mockDBManager.Setup(x => x.addDrinkTag(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns((int drinkID, int tagID) => {
+                    Item drink = fakeDBItems.FirstOrDefault(x => x.ID == drinkID);
+                    Tag tag = fakeDBTags.FirstOrDefault(x => x.ID == tagID);
+
+                    if (drink == null || tag == null) {
+                        return false;
+                    } else {
+                        drink.Tags.Add(tag);
+                        return true;
+                    }
+                });
+            
+            mockDBManager.Setup(x => x.addTag(It.IsAny<string>()))
+                .Returns((string newTagName) => {
+                    latestTagID += 1;
+                    Tag tagToAdd = new Tag {
+                        ID = latestTagID,
+                        Name = newTagName
+                    };
+
+                    fakeDBTags.Add(tagToAdd);
+                    return latestTagID;
+                });
+
+            mockDBManager.Setup(x => x.deleteTag(It.IsAny<int>()))
+                .Returns((int tagID) => {
+                    //DataBase Manager Should Automatically delete DrinkTags for Deleted Tags
+                    for (int i = 0; i < latestItemID; i++) {
+                        for (int j = 0; j < ((Drink) fakeDBItems[i]).Tags.Count(); j++) {
+                            if (fakeDBItems[i].Tags[j].ID == tagID) {
+                                fakeDBItems[i].Tags.RemoveAt(j);
+                            }
+                        }
+                    }
+
+                    fakeDBTags = fakeDBTags.Where(x => x.ID != tagID).ToList();
+                    return true;   
+                });
+
+            mockDBManager.Setup(x => x.deleteDrinkTagsByDrinkID(It.IsAny<int>()))
+                .Returns((int drinkID) => {
+                    Item drink = fakeDBItems.FirstOrDefault(x => x.ID == drinkID);
+                    if (drink != null) {
+                       drink.Tags = new List<Tag>();
+                    }
+                    return true; 
+                });
+
             mockDBManager.Setup(x => x.updateRecipe(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
                 .Returns((int recipeID, string name, string baseLiquor) => {
                     Recipe recipe = fakeDBRecipes.FirstOrDefault(x => x.ID == recipeID);
@@ -229,6 +334,7 @@ namespace BackendClassUnitTests
                 });
 
             mockDBManager.Setup(x => x.getRecipes()).Returns(fakeDBRecipes);
+
             inventory = new Inventory(mockDBManager.Object);
         }
 
@@ -317,7 +423,8 @@ namespace BackendClassUnitTests
                 BottleSize = 450,
                 Brand = "Fake Brand 4",
                 UnitsPerCase = 7,
-                Vintage = null
+                Vintage = null,
+                Tags = new List<Tag> {}
             };
             int oldDBItemCount = fakeDBItems.Count();
             int expectedReturnCode = 0;
@@ -347,6 +454,21 @@ namespace BackendClassUnitTests
             //Assert
             Assert.Equal(fakeDBItems.Count(), oldDBItemCount - 1);
             Assert.Null(fakeDBItems.FirstOrDefault(x => x.ID == itemToRemove.ID));
+            Assert.Equal(expectedReturnCode, returnCode);
+        }
+
+        [Fact]
+        public void removeItemTestFailure_ItemUsedInRecipe() {
+            //Arrange
+            var itemToRemove = fakeDBItems[2];
+            int oldDBItemCount = fakeDBItems.Count();
+            int expectedReturnCode = 1;
+
+            //Act
+            int returnCode = inventory.RemoveItem(itemToRemove);
+            //Assert
+            Assert.Equal(fakeDBItems.Count(), oldDBItemCount);
+            Assert.NotNull(fakeDBItems.FirstOrDefault(x => x.ID == itemToRemove.ID));
             Assert.Equal(expectedReturnCode, returnCode);
         }
     }
