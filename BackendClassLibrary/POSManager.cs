2747d043e0f961a3aa32f0c82de1c01de8c0f03b
Miles Mixon <mxm23@drexel.edu>
2021-04-19 18:22:16 -0400

    code for getting a list of orders and for  sending an order, sending an order is not fleshed out, and getting an order does not account for orders that have already been seen

--- BackendClassLibrary/POSManager.cs
+++ BackendClassLibrary/POSManager.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BRIM.BackendClassLibrary
 {
     public class POSManager : IPOSManager
     {
+        public JObject GetAllOrders()
+        {
+            var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/atomic_order/orders?access_token=8fe4215a-a338-4fbe-4f74-193919caa02c%22");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Accept", "application/json");
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<JObject>(response.Content);
+        }
+
+        public void SendOrder() 
+        {
+            var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/atomic_order/orders?access_token=8fe4215a-a338-4fbe-4f74-193919caa02c%22");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", 
+                "{\"orderCart\":{\"orderType\":{\"taxable\":\"false\",\"isDefault\":\"false\",\"filterCategories\":\"false\",\"isHidden\":\"false\",\"isDeleted\":\"false\"},\"groupLineItems\":\"false\"}}", 
+                ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
     }
 }
