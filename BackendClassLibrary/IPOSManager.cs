74ad26e5bab7797ccc58b9195d710f43ef7d70a1
Miles Mixon <mxm23@drexel.edu>
2021-04-19 18:49:55 -0400

    Added in functionality for filtering the orders by the times when they were created, also added method for creating orders, though it does not populate the fields of the order currently

--- BackendClassLibrary/IPOSManager.cs
+++ BackendClassLibrary/IPOSManager.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BRIM.BackendClassLibrary
 {
     public interface IPOSManager
     {
+        //Takes in a DateTime and grabs all orders on or ater that time
+        //Returns the JObject that comes from the the REST request to Clover
+        JObject GetAllOrders(DateTime time);
+
+        //This will take in information on an order in the future
+        //Most likely will be used to create orders in clover to be used for testing purposes
+        public void SendOrder();
     }
 }
--- BackendClassLibrary/POSManager.cs
+++ BackendClassLibrary/POSManager.cs
@@ -10,9 +10,11 @@ namespace BRIM.BackendClassLibrary
 {
     public class POSManager : IPOSManager
     {
-        public JObject GetAllOrders()
+        public JObject GetAllOrders(DateTime time)
         {
-            var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/atomic_order/orders?access_token=8fe4215a-a338-4fbe-4f74-193919caa02c%22");
+            var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/atomic_order/orders?filter=createdTime>=" 
+                                            + ((DateTimeOffset)time).ToUnixTimeSeconds() 
+                                            + "&access_token=8fe4215a-a338-4fbe-4f74-193919caa02c%22");
             var request = new RestRequest(Method.GET);
             request.AddHeader("Accept", "application/json");
             IRestResponse response = client.Execute(request);
@@ -27,8 +29,8 @@ namespace BRIM.BackendClassLibrary
             request.AddHeader("Accept", "application/json");
             request.AddHeader("Content-Type", "application/json");
             request.AddParameter("application/json", 
-                "{\"orderCart\":{\"orderType\":{\"taxable\":\"false\",\"isDefault\":\"false\",\"filterCategories\":\"false\",\"isHidden\":\"false\",\"isDeleted\":\"false\"},\"groupLineItems\":\"false\"}}", 
-                ParameterType.RequestBody);
+                                    "{\"orderCart\":{\"orderType\":{\"taxable\":\"false\",\"isDefault\":\"false\",\"filterCategories\":\"false\",\"isHidden\":\"false\",\"isDeleted\":\"false\"},\"groupLineItems\":\"false\"}}", 
+                                    ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
         }
     }
