9c82cd4f404fd3fbcf208d90d70bdfa2c124191b
Miles Mixon <mxm23@drexel.edu>
2021-04-19 17:03:25 -0400

    initial commit for POS connection manager class

---
+++ BackendClassLibrary/POSManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BRIM.BackendClassLibrary
+{
+    public class POSManager : IPOSManager
+    {
+    }
+}
