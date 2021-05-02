ae4ebfb605075d2646c25456af5b1adca4c6558d
Miles Mixon <mxm23@drexel.edu>
2021-05-02 00:08:48 -0400

    gets order information, but gets none of the information the documentations says it would.

--- BackendClassLibrary/POSManager.cs
+++ BackendClassLibrary/POSManager.cs
@@ -24,19 +24,38 @@ namespace BRIM.BackendClassLibrary
 
         public void SendOrder() 
         {
+            /*
+            //this code his here to manually create an order if needed
+            var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/order_types?access_token=8fe4215a-a338-4fbe-4f74-193919caa02c");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", 
+                                    "{\"taxable\":\"false\",\"isDefault\":true,\"filterCategories\":\"false\",\"isHidden\":\"false\",\"isDeleted\":\"false\",\"id\":" + 
+                                    "\"TestType\",\"labelKey\":\"TestType\",\"label\":\"TestType\",\"hoursAvailable\":\"ALL\"}", 
+                                    ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            
+            //This code is here to view order type information if needed
+            var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/order_types?access_token=8fe4215a-a338-4fbe-4f74-193919caa02c");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Accept", "application/json");
+            IRestResponse response = client.Execute(request);
+            */
+
             var client = new RestClient("https://sandbox.dev.clover.com/v3/merchants/RYBHTZ2AMPQY1/atomic_order/orders?access_token=8fe4215a-a338-4fbe-4f74-193919caa02c");
             var request = new RestRequest(Method.POST);
             request.AddHeader("Accept", "application/json");
             request.AddHeader("Content-Type", "application/json");
             request.AddParameter("application/json",
-                                    "{\"orderCart\":{\"lineItems\":[{\"item\":{\"id\":\"01\"},\"printed\":\"false\",\"exchangedLineItem\":{},\"exchanged\":\"false\"," + 
-                                    "\"modifications\":[{\"modifier\":{\"price\":\"0\",\"modifierGroup\":{}},\"id\":\"7\",\"name\":\"Test (1.5 oz)\"}],\"refunded\":\"false\"" + 
-                                    ",\"refund\":{\"orderRef\":{},\"device\":{},\"payment\":{},\"employee\":{},\"overrideMerchantTender\":{},\"serviceChargeAmount\":{}," + 
-                                    "\"germanInfo\":{},\"appTracking\":{},\"cardTransaction\":{\"extra\":{},\"vaultedCard\":{\"first6\":\"777777\",\"last4\":\"7777\"}},\"" + 
-                                    "transactionInfo\":{\"identityDocument\":{\"payment\":{}},\"isTokenBasedTx\":\"false\",\"emergencyFlag\":\"false\",\"promotionalMessage\":{},\"" + 
-                                    "sepaElvTransactionInfo\":{}},\"merchant\":{}},\"isRevenue\":\"false\",\"printGroup\":{},\"id\":\"01\",\"name\":\"TestDrink1\",\"quantitySold\":2}],\"" + 
-                                    "orderType\":{\"taxable\":\"false\",\"isDefault\":\"false\",\"filterCategories\":\"false\",\"isHidden\":\"false\",\"isDeleted\":\"false\"},\"" + 
-                                    "groupLineItems\":\"false\",\"id\":\"01\",\"currency\":\"USD\",\"title\":\"test1\"}}", 
+                                    "{\"orderCart\":{\"lineItems\":[{\"item\":{\"id\":\"V341Y5136FFMP\"},\"printed\":\"false\",\"exchangedLineItem\":{},\"exchanged\":\"false\"," + 
+                                    "\"modifications\":[{\"modifier\":{\"price\":\"0\",\"modifierGroup\":{}},\"id\":\"7\",\"name\":\"Test (1.5 oz)\"}],\"refunded\":\"false\"," + 
+                                    "\"refund\":{\"orderRef\":{},\"device\":{},\"payment\":{},\"employee\":{},\"overrideMerchantTender\":{},\"serviceChargeAmount\":{},\"germanInfo\"" + 
+                                    ":{},\"appTracking\":{},\"cardTransaction\":{\"extra\":{},\"vaultedCard\":{\"first6\":\"777777\",\"last4\":\"7777\"}},\"transactionInfo\":{" + 
+                                    "\"identityDocument\":{\"payment\":{}},\"isTokenBasedTx\":\"false\",\"emergencyFlag\":\"false\",\"promotionalMessage\":{},\"sepaElvTransactionInfo\"" +
+                                    ":{}},\"merchant\":{}},\"isRevenue\":\"false\",\"printGroup\":{},\"name\":\"TestDrink1\",\"quantitySold\":2}],\"orderType\":{\"taxable\":true," + 
+                                    "\"isDefault\":true,\"filterCategories\":\"false\",\"isHidden\":\"false\",\"isDeleted\":\"false\",\"label\":\"Test\",\"id\":\"1Z288B5A2B0F8\"," + 
+                                    "\"labelKey\":\"com.clover.order.type.dine_in\",\"hoursAvailable\":\"BUSINESS\",\"systemOrderTypeId\":\"DINE-IN-TYPE\"}," + 
+                                    "\"groupLineItems\":\"false\",\"currency\":\"USD\",\"title\":\"test1\"}}", 
                                     ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
 
