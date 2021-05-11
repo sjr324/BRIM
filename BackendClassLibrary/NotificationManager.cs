2a0eb64c6fe4bdd3cdd600cb886f4c1d5af29235
Alex Serebriiskii <alex.serebriiskii@gmail.com>
2021-05-10 21:52:12 -0400

    done notification dialog

--- BackendClassLibrary/NotificationManager.cs
+++ BackendClassLibrary/NotificationManager.cs
@@ -8,17 +8,31 @@ namespace BRIM.BackendClassLibrary
     public static class NotificationManager
     {
         private static int currentID = 0;
+        private static Notification latest;
         private static List<Notification> notifications = new List<Notification>();
 
         public static void AddNotification(string message)
         {
-            notifications.Add(new Notification(currentID, message));
+            Notification notification=new Notification(currentID, message);
+            notifications.Add(notification);
+            latest = notification;
             currentID++;
+
         }
 
         public static void RemoveNotification(int id)
         {
            notifications.Remove(notifications.Where(n => n.ID == id).First());
+        }
+        
+        public static void MarkRead(int id)
+        {
+            notifications.Where(n => n.ID == id).First().StatusCode=1;
+        }
+
+        public static Notification GetMostRecent()
+        {
+            return latest;
         }
     }
 }
--- Content/components/NavDrawer.jsx
+++ Content/components/NavDrawer.jsx
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
+import NtfcnDialog from './notifications/NtfcnDialog.jsx';
 import MailIcon from "@material-ui/icons/Mail";
 import { withRouter } from "react-router-dom";
 
@@ -50,6 +51,7 @@ const NavDrawer = props => {
             </ListItem>
           );
         })}
+        <NtfcnDialog></NtfcnDialog>
       </List>
     </MUIDrawer>
   );
